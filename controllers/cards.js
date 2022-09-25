95e37ad5e3469724eeddf7df2013b24e503d65ef
Dmitry Kushnerik <kushnerik.dmitry@yandex.ru>
2022-09-25 20:25:32 +0700

    feat: начальные файлы

---
+++ controllers/cards.js
@@ -0,0 +1,87 @@
+const VALIDATION_ERROR = 400;
+const NOT_FOUND_ERROR = 404;
+const DEFAULT_ERROR = 500;
+
+const Card = require('../models/card');
+
+module.exports.getAllCards = (req, res) => {
+  Card.find({})
+    .then((cards) => res.send({ data: cards }))
+    .catch(() => res.status(DEFAULT_ERROR).send({ message: 'На сервере произошла ошибка' }));
+};
+
+module.exports.createCard = (req, res) => {
+  const { name, link } = req.body;
+  Card.create({ name, link, owner: req.user._id })
+    .then((card) => res.send({ data: card }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(VALIDATION_ERROR).send({ message: 'Переданы некорректные данные при создании карточки.' });
+      }
+
+      return res.status(DEFAULT_ERROR).send({ message: 'На сервере произошла ошибка' });
+    });
+};
+
+module.exports.deleteCard = (req, res) => {
+  Card.findByIdAndRemove(req.params.cardId)
+    .then((card) => {
+      if (card === null) {
+        return res.status(NOT_FOUND_ERROR).send({ message: 'Карточка с указанным _id не найдена' });
+      }
+      return res.send({ data: card });
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(NOT_FOUND_ERROR).send({ message: 'Карточка с указанным _id не найдена' });
+      }
+
+      return res.status(DEFAULT_ERROR).send({ message: 'На сервере произошла ошибка' });
+    });
+};
+
+module.exports.likeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+    { new: true },
+  )
+    .then((card) => {
+      if (card === null) {
+        return res.status(NOT_FOUND_ERROR).send({ message: 'Передан несуществующий _id карточки' });
+      }
+      return res.send({ data: card });
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(VALIDATION_ERROR).send({ message: 'Переданы некорректные данные для постановки/снятии лайка.' });
+      }
+      if (err.name === 'CastError') {
+        return res.status(NOT_FOUND_ERROR).send({ message: 'Передан несуществующий _id карточки' });
+      }
+      return res.status(DEFAULT_ERROR).send({ message: 'На сервере произошла ошибка' });
+    });
+};
+
+module.exports.dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } }, // убрать _id из массива
+    { new: true },
+  )
+    .then((card) => {
+      if (card === null) {
+        return res.status(NOT_FOUND_ERROR).send({ message: 'Передан несуществующий _id карточки' });
+      }
+      return res.send({ data: card });
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(VALIDATION_ERROR).send({ message: 'Переданы некорректные данные для постановки/снятии лайка.' });
+      }
+      if (err.name === 'CastError') {
+        return res.status(NOT_FOUND_ERROR).send({ message: 'Передан несуществующий _id карточки' });
+      }
+      return res.status(DEFAULT_ERROR).send({ message: 'На сервере произошла ошибка' });
+    });
+};
---
+++ controllers/users.js
@@ -0,0 +1,86 @@
+const VALIDATION_ERROR = 400;
+const NOT_FOUND_ERROR = 404;
+const DEFAULT_ERROR = 500;
+
+const User = require('../models/user');
+
+module.exports.getAllUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch(() => res.status(DEFAULT_ERROR).send({ message: 'На сервере произошла ошибка' }));
+};
+
+module.exports.getUserById = (req, res) => {
+  User.findById(req.params.userId)
+    .then((user) => {
+      if (!user) {
+        return res.status(NOT_FOUND_ERROR).send({ message: 'Пользователь по указанному _id не найден' });
+      }
+      return res.send({ data: user });
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(NOT_FOUND_ERROR).send({ message: 'Пользователь по указанному _id не найден' });
+      }
+
+      return res.status(DEFAULT_ERROR).send({ message: 'На сервере произошла ошибка' });
+    });
+};
+
+module.exports.createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(VALIDATION_ERROR).send({ message: 'Переданы некорректные данные при создании пользователя' });
+      }
+
+      return res.status(DEFAULT_ERROR).send({ message: 'На сервере произошла ошибка' });
+    });
+};
+
+module.exports.updateUserInfo = (req, res) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    { new: true, runValidators: true },
+  )
+    .then((user) => {
+      if (!user) {
+        return res.status(NOT_FOUND_ERROR).send({ message: 'Пользователь по указанному _id не найден' });
+      }
+      return res.send({ data: user });
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(VALIDATION_ERROR).send({ message: 'Переданы некорректные данные при обновлении профиля' });
+      }
+      if (err.name === 'DocumentNotFoundError') {
+        return res.status(NOT_FOUND_ERROR).send({ message: 'Пользователь с указанным _id не найден' });
+      }
+      return res.status(DEFAULT_ERROR).send({ message: 'На сервере произошла ошибка' });
+    });
+};
+
+module.exports.updateUserAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar },
+    { new: true, runValidators: true },
+  )
+    .then((user) => {
+      if (!user) {
+        return res.status(NOT_FOUND_ERROR).send({ message: 'Пользователь по указанному _id не найден' });
+      }
+      return res.send({ data: user });
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(VALIDATION_ERROR).send({ message: 'Переданы некорректные данные при обновлении профиля' });
+      }
+      return res.status(DEFAULT_ERROR).send({ message: 'На сервере произошла ошибка' });
+    });
+};
