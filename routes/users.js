374f619eff944469a93d4b836db6cf9e91cad77e
Dmitry Kushnerik <kushnerik.dmitry@yandex.ru>
2022-10-04 23:48:26 +0700

    feat: проверка автотестов 2.5

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,4 @@
-// const { celebrate, Joi } = require('celebrate');
+const { celebrate, Joi } = require('celebrate');
 const router = require('express').Router();
 const {
   getAllUsers, getUserById, updateUserInfo, updateUserAvatar, getInfoAboutMe,
@@ -7,7 +7,17 @@ const {
 router.get('/', getAllUsers);
 router.get('/:userId', getUserById);
 router.get('/me/', getInfoAboutMe);
-router.patch('/me/', updateUserInfo);
-router.patch('/me/avatar', updateUserAvatar);
+router.patch('/me/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), updateUserInfo);
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    // eslint-disable-next-line no-useless-escape
+    avatar: Joi.string().regex(/^https?:\/\/(www\.)?[a-zA-Z0-9\-\._~:\/?#\[\]@!$&'()*+,;=]+\#?/),
+  }),
+}), updateUserAvatar);
 
 module.exports = router;
