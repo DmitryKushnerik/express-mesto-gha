fb8c0032d0680282f2017d95d8df4e5ede38eb7f
Dmitry Kushnerik <kushnerik.dmitry@yandex.ru>
2022-10-05 21:42:05 +0700

    fix: исправить ошибку с авторизацией 2

--- controllers/users.js
+++ controllers/users.js
@@ -125,7 +125,7 @@ module.exports.login = (req, res, next) => {
         'some-secret-key',
         { expiresIn: '7d' },
       );
-      res.send(token);
+      res.send({ user, token });
     })
     .catch(() => next(new AuthorisationError('Произошла ошибка авторизации')));
 };
