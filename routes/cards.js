bf3d8dbbc71b0b4e14112d23b3cb6e0a7eb1eb88
Dmitry Kushnerik <kushnerik.dmitry@yandex.ru>
2022-10-06 20:58:12 +0700

    feat: проверка карточки с JOI

--- routes/cards.js
+++ routes/cards.js
@@ -1,3 +1,4 @@
+const { celebrate, Joi } = require('celebrate');
 const router = require('express').Router();
 const {
   getAllCards, createCard, deleteCard, likeCard, dislikeCard,
@@ -5,7 +6,12 @@ const {
 
 router.get('/', getAllCards);
 
-router.post('/', createCard);
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    link: Joi.string().required().regex(/^https?:\/\/(www\.)?[a-zA-Z0-9\-\._~:\/?#\[\]@!$&'()*+,;=]+\#?/),
+  }),
+}), createCard);
 
 router.delete('/:cardId', deleteCard);
 
