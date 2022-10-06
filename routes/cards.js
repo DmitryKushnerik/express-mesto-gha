06770033b6c30741ee5ab86bb21ec3bae82a61e3
Dmitry Kushnerik <kushnerik.dmitry@yandex.ru>
2022-10-06 21:16:54 +0700

    fix: валидация карточки с Joi

--- routes/cards.js
+++ routes/cards.js
@@ -9,6 +9,7 @@ router.get('/', getAllCards);
 router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
+    // eslint-disable-next-line no-useless-escape
     link: Joi.string().required().regex(/^https?:\/\/(www\.)?[a-zA-Z0-9\-\._~:\/?#\[\]@!$&'()*+,;=]+\#?/),
   }),
 }), createCard);
