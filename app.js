cfc30bc3c15ebf9e872e8a7af6be6e2c512d011f
Dmitry Kushnerik <kushnerik.dmitry@yandex.ru>
2022-10-11 18:05:15 +0700

    feat: добавить логгеры для сервера

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { /* celebrate, Joi, */ errors } = require('celebrate');
 const routes = require('./routes/index');
-/* const NotFoundError = require('./errors/NotFoundError');
-const { createUser, login } = require('./controllers/users');
-const auth = require('./middlewares/auth');
-const { urlTemplate } = require('./utils/validation'); */
 const errorHandler = require('./middlewares/errorHandler');
 
 const { PORT = 3000 } = process.env;
